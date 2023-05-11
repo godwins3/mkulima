6be29ea46a32e71df66a488b041f6fd66cb7a216
godwins3 <praisegodwins4@gmail.com>
2023-05-11 17:18:48 +0300

    adding africastalking

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 # Importing essential libraries and modules
 
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template, request, Markup, redirect
 import numpy as np
 import pandas as pd
 from utils.disease import disease_dic
@@ -13,6 +13,8 @@ import torch
 from torchvision import transforms
 from PIL import Image
 from utils.model import ResNet9
+from app import mpesa, app
+from app.airtime import send_airtime
 # ==============================================================================================
 
 # -------------------------LOADING THE TRAINED MODELS -----------------------------------------------
@@ -258,6 +260,37 @@ def disease_prediction():
         except:
             pass
     return render_template('disease.html', title=title)
+
+@app.route('/lipa-na-mpesa')
+def lipa_na_mpesa(id):    
+    access_token = mpesa.MpesaAccessToken.validated_mpesa_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {'Authorization': 'Bearer %s' % access_token}
+    mpesa_request = {
+        'BusinessShortCode': mpesa.LipaNaMpesaPassword.business_short_code,   # org receiving funds
+        'Password': mpesa.LipaNaMpesaPassword.decode_online_password,         # used to encrypt the request
+        'Timestamp':mpesa.LipaNaMpesaPassword.lipa_time,                      # transaction time
+        'TransactionType': 'CustomerPayBillOnline',
+        'Amount': 1,                                                          # transaction amount
+        'PartyA': int((current_user.phone).replace('+', '')),                 # MSISDN sending the funds
+        'PartyB': mpesa.LipaNaMpesaPassword.business_short_code,               # org receiving the funds
+        'PhoneNumber': int((current_user.phone).replace('+', '')),            # MSISDN sending the funds
+        'CallBackURL': 'https://sandbox.safaricom.co.ke/mpesa/',
+        'AccountReference': 'mkulima',
+        'TransactionDesc': 'testing stk push for ecommerce app'
+    }
+    try:
+        response = requests.post(api_url, json=mpesa_request, headers=headers)
+        print(response.text, f'\n\nStatus: {response.status_code}')
+
+        # Update payment status in database
+        
+        # Send airtime after purchase
+        send_airtime()
+
+    except Exception as e:
+        print(f'Error: \n\n {e}')    
+    return redirect(url_for('dashboard_customer'))
 
 
 # ===============================================================================================
---
+++ main.py
@@ -0,0 +1,5 @@
+from app import*
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug=False)
